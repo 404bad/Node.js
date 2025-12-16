1d9524df78ec2de170c0c0aeb7483f36f0ae094a
404bad <kailashbaduatwork@gmail.com>
2025-12-16 23:58:14 +0545

    ix(users-api): correct app.route() usage for GET, PATCH, DELETE

--- 9.RESTApis/Project-01/index.js
+++ 9.RESTApis/Project-01/index.js
@@ -30,15 +30,7 @@ app.get("/api/users", (req, res) => {
 });
 
 // 2. get /api/users/:id  dynamic path parameter
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  // console.log(typeof(id));// this is a string so convert it to  number data typer
-  const user = users.find((user) => user.id === id);
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-  return res.json(user);
-});
+// app
 
 //3. post /api/users
 app.post("/api/users", (req, res) => {
@@ -57,52 +49,66 @@ app.post("/api/users", (req, res) => {
   });
 });
 
-//4. patch /api/users/:id
-app.patch("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const body = req.body;
-  const userIndex = users.findIndex((user) => user.id === id);
-  if (userIndex === -1) {
-    return res.status(404).json({ message: "User not found" });
-  }
-  users[userIndex] = {
-    ...users[userIndex],
-    ...body,
-  };
-  //   console.log(userIndex.id);
-  fs.writeFile(dataPath, JSON.stringify(users), (err) => {
-    if (err) {
-      return res.status(500).json({ message: "Failed to update user" });
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    // console.log(typeof(id));// this is a string so convert it to  number data typer
+    const user = users.find((user) => user.id === id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    return res.json(users[userIndex]);
-  });
-});
-
-//4. delete /api/users/:id
-app.delete("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-
-  // get index of the user
-  const userIndex = users.findIndex((user) => user.id === id);
-
-  if (userIndex === -1) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  // remove the user from array
-  const deletedUser = users.splice(userIndex, 1);
-
-  // write updated array to JSON file
-  fs.writeFile(dataPath, JSON.stringify(users, null, 2), (err) => {
-    if (err) {
-      return res.status(500).json({ message: "Failed to delete the user" });
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const body = req.body;
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return res.status(404).json({ message: "User not found" });
     }
-    return res.json({
-      message: "User deleted successfully",
-      user: deletedUser[0],
+    users[userIndex] = {
+      ...users[userIndex],
+      ...body,
+    };
+    //   console.log(userIndex.id);
+    fs.writeFile(dataPath, JSON.stringify(users), (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Failed to update user" });
+      }
+      return res.json(users[userIndex]);
+    });
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+
+    // get index of the user
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // remove the user from array
+    const deletedUser = users.splice(userIndex, 1);
+
+    // write updated array to JSON file
+    fs.writeFile(dataPath, JSON.stringify(users, null, 2), (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Failed to delete the user" });
+      }
+      return res.json({
+        message: "User deleted successfully",
+        user: deletedUser[0],
+      });
     });
   });
-});
+
+//4. patch /api/users/:id
+// app
+
+//4. delete /api/users/:id
+// app
 
 //listen the server on port
 app.listen(PORT, () => {
